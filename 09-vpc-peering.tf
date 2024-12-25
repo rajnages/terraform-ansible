ff10f6805909a1d7fb766ef6fcda2c0f75e2e527
Rajavarapu Nageswarao <nagesrajavarapu@gmail.com>
2024-12-25 19:14:30 +0530

    new setup is configured

--- 09-vpc-peering.tf
+++ 09-vpc-peering.tf
@@ -1,9 +1,9 @@
 data "aws_vpc" "ansible_vpc" {
-  id = "vpc-0ff098e3c18846e26"
+  id = "vpc-04477a852827d2906"
 }
 
 data "aws_route_table" "ansible_vpc_rt" {
-  subnet_id = "subnet-0c8447788ae9b1bba"
+  subnet_id = "subnet-0ee62abfb3853de2f"
   #If subnet_id giving errors use route table id as below
   #route_table_id = data.aws_route_table.ansible_vpc_rt.id
 }
