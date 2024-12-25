62e9b2f68281761eef509b1a5eb747f5368a135b
Rajavarapu Nageswarao <nagesrajavarapu@gmail.com>
2024-12-25 19:52:57 +0530

    new setup is configured

--- 09-vpc-peering.tf
+++ 09-vpc-peering.tf
@@ -1,9 +1,9 @@
 data "aws_vpc" "ansible_vpc" {
-  id = "vpc-0c88d32bf5ca84888"
+  id = "vpc-09ffb95f78f57137d"
 }
 
 data "aws_route_table" "ansible_vpc_rt" {
-  subnet_id = "subnet-0af5c5d72fc451d2f"
+  subnet_id = "subnet-0f62cb5d652d1e640"
   #If subnet_id giving errors use route table id as below
   #route_table_id = data.aws_route_table.ansible_vpc_rt.id
 }
@@ -27,7 +27,7 @@ resource "aws_vpc_peering_connection" "ansible-vpc-peering" {
 
 resource "aws_route" "peering-to-ansible-vpc" {
   route_table_id            = aws_route_table.terraform-public.id
-  destination_cidr_block    = "172.31.0.0/16"
+  destination_cidr_block    = "10.0.0.0/16"
   vpc_peering_connection_id = aws_vpc_peering_connection.ansible-vpc-peering.id
   #depends_on                = [aws_route_table.terraform-public]
 }
