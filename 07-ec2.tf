374d175480191b4eab94c13e768d626e2e93c524
Rajavarapu Nageswarao <nagesrajavarapu@gmail.com>
2024-12-25 09:48:49 +0530

    Changes are done

--- 07-ec2.tf
+++ 07-ec2.tf
@@ -1,20 +1,10 @@
-data "aws_ami" "my_ami" {
-  most_recent = true
-  filter {
-    name   = "tags:Name"
-    values = ["Ansible-Controller"]
-  }
-  owners      = ["533267248538"]
-}
-
 resource "aws_instance" "webservers" {
-  #count                       = local.new_environment == "production" ? 3 : 1
   count                       = 3
-  ami                         = data.aws_ami.my_ami.id
+  ami                         = "ami-0e2c8caa4b6378d8c"
   instance_type               = lookup(var.instance_type, local.new_environment)
   key_name                    = var.key_name
   subnet_id                   = element(aws_subnet.public-subnets.*.id, count.index)
-  vpc_security_group_ids      = ["${aws_security_group.allow_all.id}"]
+  vpc_security_group_ids      = [aws_security_group.allow_all.id]
   associate_public_ip_address = true
   tags = {
     Name              = "${var.vpc_name}-PublicServer-${count.index + 1}"
